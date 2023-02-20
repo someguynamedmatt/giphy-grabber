97f7eecf49f5eb0c1c8a82f35d9ba258e44ef7db
Matt Young <dev@mttyng.com>
2023-02-19 16:54:16 -0800

    refactor: refactored the resize fn for gifs

--- src/components/GifGrid/index.js
+++ src/components/GifGrid/index.js
@@ -4,67 +4,95 @@ import { Grid, GridWrapper } from './styles'
 import { Gif } from '@/components'
 import { GifContext } from '@/providers'
 import { useIntersection } from 'react-use'
+import { useDebounce } from 'react-use'
 
-function resizeMasonryItem(item) {
-  const grid = document?.getElementsByClassName('grid')[0]
-  const rowGap = parseInt(globalThis?.window.getComputedStyle(grid).getPropertyValue('row-gap'))
-  const rowHeight = parseInt(
-    globalThis?.window.getComputedStyle(grid).getPropertyValue('grid-auto-rows')
-  )
-
+function resizeMasonryItem({ item, rowGap, rowHeight }) {
   const rowSpan = Math.ceil(
     (item.querySelector('.grid-item').getBoundingClientRect().height + rowGap) /
       (rowHeight + rowGap)
   )
-
   item.style.gridRowEnd = 'span ' + rowSpan
 }
 
-function waitForImages() {
-  const allItems = document?.getElementsByClassName('grid-item-wrapper')
-  const gridWrapper = document?.getElementsByClassName('grid-wrapper')[0]
-  const gridWrapperWidth = globalThis?.window?.getComputedStyle(gridWrapper).width
-  for (var i = 0; i < allItems.length; i++) {
-    resizeMasonryItem(allItems[i])
-  }
-}
-function resizeAllMasonryItems() {
-  var allItems = document?.getElementsByClassName('grid-item-wrapper')
-  for (var i = 0; i < allItems.length; i++) {
-    resizeMasonryItem(allItems[i])
-  }
-}
-
+/* function waitForImages() {
+ *   const allItems = document?.getElementsByClassName('grid-item-wrapper')
+ *   const gridWrapper = document?.getElementsByClassName('grid-wrapper')[0]
+ *   const gridWrapperWidth = globalThis?.window?.getComputedStyle(gridWrapper).width
+ *   for (var i = 0; i < allItems.length; i++) {
+ *     resizeMasonryItem(allItems[i])
+ *   }
+ * }
+ *  */
+/* function resizeAllMasonryItems() {
+ *   const grid = document?.getElementsByClassName('grid')[0]
+ *   const allItems = document?.getElementsByClassName('grid-item-wrapper')
+ *   const rowGap = parseInt(globalThis?.window.getComputedStyle(grid).getPropertyValue('row-gap'))
+ *   const rowHeight = parseInt(
+ *     globalThis?.window.getComputedStyle(grid).getPropertyValue('grid-auto-rows')
+ *   )
+ *
+ *   for (let i = 0; i < allItems.length; i++) {
+ *     resizeMasonryItem({ item: allItems[i], rowGap, rowHeight })
+ *   }
+ * }
+ *  */
 const GifGrid = () => {
   const { gifs, fetchWithQuery, currentSearchTerm, setGifs } = GifContext.useContainer()
   const pageRef = useRef(1)
   const ref = useRef(null)
   const intersection = useIntersection(ref, { root: null, rootMargin: '0px', threshold: 0.5 })
 
-  useEffect(() => {
-    if (globalThis?.window) {
-      globalThis?.window.addEventListener('resize', waitForImages)
+  const resizeAllMasonryItems = () => {
+    const grid = document?.getElementsByClassName('grid')[0]
+    const allItems = document?.getElementsByClassName('grid-item-wrapper')
+    const rowGap = parseInt(globalThis?.window.getComputedStyle(grid).getPropertyValue('row-gap'))
+    const rowHeight = parseInt(
+      globalThis?.window.getComputedStyle(grid).getPropertyValue('grid-auto-rows')
+    )
+
+    for (let i = 0; i < allItems.length; i++) {
+      resizeMasonryItem({ item: allItems[i], rowGap, rowHeight })
     }
-    return () => globalThis?.window.removeEventListener('resize', waitForImages)
-  }, [globalThis?.window, waitForImages])
+  }
 
-  useEffect(() => {
-    /* if (globalThis?.window) waitForImages() */
-  }, [waitForImages])
-
-  useEffect(() => {
-    const fn = async () => {
-      if (intersection?.intersectionRatio) {
-        console.log('INTERSECTION')
-        pageRef.current += 1
-        const newBatch = await fetchWithQuery({ query: currentSearchTerm, page: pageRef.current })
-        console.log('INTERSECTION', newBatch)
-        if (newBatch?.length) {
-          setGifs([...gifs, ...newBatch])
-        }
+  const fn = async () => {
+    if (intersection?.intersectionRatio) {
+      console.log('INTERSECTION')
+      pageRef.current += 1
+      const newBatch = await fetchWithQuery({ query: currentSearchTerm, page: pageRef.current })
+      console.log('INTERSECTION', newBatch)
+      if (newBatch?.length) {
+        setGifs([...gifs, ...newBatch])
       }
     }
-    fn()
+  }
+
+  /* useDebounce(fn, 150, [fn]) */
+
+  useEffect(() => {
+    if (globalThis?.window) {
+      globalThis?.window.addEventListener('resize', resizeAllMasonryItems)
+    }
+    return () => globalThis?.window.removeEventListener('resize', resizeAllMasonryItems)
+  }, [globalThis?.window, resizeAllMasonryItems])
+
+  useEffect(() => {
+    if (globalThis?.window) resizeAllMasonryItems()
+  }, [resizeAllMasonryItems])
+
+  useEffect(() => {
+    /* const fn = async () => {
+     *   if (intersection?.intersectionRatio) {
+     *     console.log('INTERSECTION')
+     *     pageRef.current += 1
+     *     const newBatch = await fetchWithQuery({ query: currentSearchTerm, page: pageRef.current })
+     *     console.log('INTERSECTION', newBatch)
+     *     if (newBatch?.length) {
+     *       setGifs([...gifs, ...newBatch])
+     *     }
+     *   }
+     * }
+     * fn() */
   }, [intersection?.intersectionRatio])
 
   return (
