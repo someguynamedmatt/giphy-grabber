1782b54b9125865b70ac6a32693f1c326ab46332
Matt Young <dev@mttyng.com>
2023-02-19 19:48:46 -0800

    refactor: column width fixes for Grid

--- src/components/GifGrid/styles.js
+++ src/components/GifGrid/styles.js
@@ -1,20 +1,23 @@
 import styled from 'styled-components'
 
-export const GridWrapper = styled.div``
+export const GridWrapper = styled.div`
+  height: 100%;
+`
 
 export const Grid = styled.div`
   display: grid;
   grid-gap: 1px;
-  /* grid-template-columns: repeat(4, minmax(200px, 1fr)); */
   grid-auto-rows: 0;
   max-width: 1280px;
   width: 100%;
+  height: auto;
+  /* grid-template-rows: auto; */
 
-  @media screen and (min-width: 1042px) {
+  @media screen and (min-width: 1317px) {
     grid-template-columns: repeat(4, minmax(200px, 1fr));
   }
 
-  @media screen and (max-width: 1041px) {
+  @media screen and (max-width: 1317px) {
     grid-template-columns: repeat(3, minmax(200px, 1fr));
   }
 
